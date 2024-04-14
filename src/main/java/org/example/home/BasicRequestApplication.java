cb7deefa20b15caec4f52de875d1c809e00294c3
Andrew Kurylyk <andronniy7@gmail.com>
2024-04-14 19:04:01 +0300

    First

---
+++ src/main/java/org/example/home/BasicRequestApplication.java
@@ -0,0 +1,13 @@
+package org.example.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicRequestApplication.class, args);
+    }
+
+}
