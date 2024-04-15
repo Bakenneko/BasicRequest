7ac264221ccc2afee349fda7c3601fc897bb2e52
Andrew Kurylyk <andronniy7@gmail.com>
2024-04-15 21:17:32 +0300

    First

--- src/main/java/org/example/home/controllers/MainController.java
+++ src/main/java/org/example/home/controllers/MainController.java
@@ -1,9 +1,30 @@
 package org.example.home.controllers;
 
+import org.example.home.models.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MainController {
+    List<Customer> customers = new ArrayList<>();
 
+    public MainController() {
+        customers.add(new Customer(1,"Jack"));
+        customers.add(new Customer(2,"Nick"));
+        customers.add(new Customer(3,"Piter"));
+    }
+
+    @GetMapping({"/hello","/"})
+    public List<Customer> hello() {
+        return customers;
+    }
+
+//    @GetMapping({"/hello","/"})
+//    public String hello() {
+//        return "Hello Andrew;";
+//    }
 
 }
---
+++ src/main/java/org/example/home/models/Customer.java
@@ -0,0 +1,14 @@
+package org.example.home.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Customer {
+    private int id;
+    private String name;
+}
